f5ab131e1a27dbc15ae3809ebdfb22e5ad9f06b2
MD GULAM RASUL KHAN <130956007+mdgulamrasulkhan@users.noreply.github.com>
2024-06-14 13:56:49 +0000

    some datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+ /* 
+ "use strict" //treat all js code as newer version
+
+ alert(3+3) //we are using node js no web browser 
+
+ we have to make code more readability so we can write 
+ console.log("hello");
+ console.log("farhan");
+ //instead of  
+ console.log("hello"); console.log("farhan");
+ */
+let age = 18
+let isloggedin = false
+let state;
+
+// numer 2 to the ower 53
+//bigint
+//string => ""
+//boolean = true/false
+//null = standalone value
+//undefine =>
+//symbol = unique
+//object
+
+console.log(typeof isloggedin);
