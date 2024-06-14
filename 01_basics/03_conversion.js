6b42777aac393014c96830774c59f4430dd5a684
MD GULAM RASUL KHAN <130956007+mdgulamrasulkhan@users.noreply.github.com>
2024-06-14 14:28:40 +0000

    new

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,10 @@
+let score = "farhan"
+
+console.log(typeof score);
+console.log(typeof (score));
+console.log("hello woeld");
+
+console.log("hello");
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
