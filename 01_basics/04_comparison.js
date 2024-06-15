419dcec3fe4166b0b3994a252c9dc1c228149e83
MD GULAM RASUL KHAN <130956007+mdgulamrasulkhan@users.noreply.github.com>
2024-06-15 07:56:17 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1)    *trated js string to number automatically
+// console.log(1<"02")    *trated js string to number automatically
+// so output is true
+
+// console.log(null<0);
+// console.log(null==0);
+// console.log(null<=0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+//most of the time we ignore this type of convrsion
+
+//   === use for strictly check
+console.log("2"===2);
