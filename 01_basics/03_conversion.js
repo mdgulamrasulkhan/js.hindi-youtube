0a1a20f997522374657e31a9737742f0d3f31099
MD GULAM RASUL KHAN <130956007+mdgulamrasulkhan@users.noreply.github.com>
2024-06-15 06:50:15 +0000

    conversion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,10 +1,26 @@
 let score = "farhan"
 
-console.log(typeof score);
-console.log(typeof (score));
-console.log("hello woeld");
+// console.log(typeof score);
+// console.log(typeof (score));
+// console.log("hello woeld");
 
-console.log("hello");
+// console.log("hello");
 let valueInNumber = Number (score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// "33"=>33
+// "33abc" => NaN
+// true =>1 ; false => 0;
+let isloggedIn = "farhan"
+
+let booleanloggedIn = Boolean(isloggedIn)
+// console.log(booleanloggedIn);
+/*
+1 => true; 0=> false;
+"" => false, "hii" => true
+*/
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
