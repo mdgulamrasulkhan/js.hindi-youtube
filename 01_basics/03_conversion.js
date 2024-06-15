07df6fb1ade90b204ad978ab9cf4a59ccc6a1e98
MD GULAM RASUL KHAN <130956007+mdgulamrasulkhan@users.noreply.github.com>
2024-06-15 07:19:53 +0000

    improve

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,4 +1,4 @@
-let score = "farhan"
+/*let score = "farhan"
 
 // console.log(typeof score);
 // console.log(typeof (score));
@@ -18,9 +18,44 @@ let booleanloggedIn = Boolean(isloggedIn)
 /*
 1 => true; 0=> false;
 "" => false, "hii" => true
-*/
+
 let someNumber = 33
 let StringNumber = String(someNumber)
 console.log(StringNumber);
 console.log(typeof StringNumber);
+*/
 
+//*******************operations***********************************//
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "farhan"
+// let str2 = " khan"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1+ "2");
+// console.log("1"+ 3 + 2);
+// console.log(1 + 2 +"3");
+
+//so if strig comes first then js treat all the value as string viseversa for Number 
+
+//console.log(true+); not allowed
+// console.log(+true);
+// console.log(+"");
+
+let gamecounter = 100
+++gamecounter
+console.log(gamecounter);
+gamecounter++
+console.log(gamecounter);
+// read the resources from mdn of prefix and postfix operator
