82ff0e2bc46322cd672bb0b8211528e86a84436c
MD GULAM RASUL KHAN <130956007+mdgulamrasulkhan@users.noreply.github.com>
2024-06-15 13:13:51 +0000

    change in operation

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,24 @@
+// primitive datatypes -
+//Number, boolean, string, null, undefined,bigint,symbol
+// const score = 100
+ const scorevalue = 100.3
+
+// const loggedIn = false
+// const outsideTemp = null
+// let userMail;
+
+// const id= Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id===anotherId);
+// const bignumber = 458768768767857646565343378n
+// console.log(bignumber);
+// // reference (non primitive)
+// //Array,objects,functions
+const heros = ["shaktiman","balveer","doha"]
+console.log(typeof scorevalue);
+
+let myobj = {
+    Name: "farhan",
+    Age : 22,
+
+}
