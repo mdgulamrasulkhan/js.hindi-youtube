70a9869e4035553fc3f662ccc34468be48dac2eb
MD GULAM RASUL KHAN <130956007+mdgulamrasulkhan@users.noreply.github.com>
2024-06-16 14:14:56 +0000

    string

---
+++ 01_basics/06_string.js
@@ -0,0 +1,28 @@
+// // const name = 'hites'
+// // const repo = 50
+// // // console.log(name + repo +"value");/
+// // console.log(`hello my name is ${name} my repo value is ${repo}` );
+
+ const gameName = new String('hi t es h hc')
+//  console.log(gameName[4]);
+//  console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// // console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherstring = gameName.slice(0,5)
+// console.log(anotherstring);
+
+// const newStringOne = "      farhan   "
+// console.log(newStringOne.trim());
+// console.log(newStringOne);
+
+// const url = "https//:farhankhan/farhan%20helloorld"
+// console.log(url.replace('%20','+'))
+// console.log(url.includes('farhan'))
+// console.log(url.includes('farhaan'))
+console.log(gameName.split(" "));
--- 01_basics/string.js
+++
@@ -1,8 +0,0 @@
-// const name = 'hites'
-// const repo = 50
-// // console.log(name + repo +"value");/
-// console.log(`hello my name is ${name} my repo value is ${repo}` );
-
-const gameName = new String('hiteshhc')
-console.log(gameName[4]);
-console.log(gameName.__proto__);
