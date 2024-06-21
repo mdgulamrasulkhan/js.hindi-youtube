492e491a099780ad9b87a947a7c364fadf96c39d
MD GULAM RASUL KHAN <130956007+mdgulamrasulkhan@users.noreply.github.com>
2024-06-21 05:00:48 +0000

    switch_operation

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,43 @@
+//syntax of switch operation
+
+// switch () {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+switch (3) {
+    case  1 : console.log("one");
+    case 2 : console.log("two");
+    // case 3 : console.log("three");
+        
+        break;
+
+    default:
+        break;
+}
+
+const month = "feb"
+switch (month)
+{
+    case "jan" :
+                console.log("january");
+                break;
+     case "feb":
+               console.log("february");
+               break;
+     case "mar":
+               console.log("march");
+               break;
+     case "apr":
+               console.log("april");
+   
+   break;
+
+default:
+   break;
+}
