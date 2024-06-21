3e15199cded362febad33ed33de2cc213dbfa99e
MD GULAM RASUL KHAN <130956007+mdgulamrasulkhan@users.noreply.github.com>
2024-06-21 14:21:21 +0000

    while , do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,19 @@
+// let index = 2
+
+// while (index<=20) {
+//     console.log(`value of index is ${index}`);
+//     index = index+2
+// }
+
+// let myArray = ["masoom" , "farhan" , "mister"]
+// let index = 0
+// while (index < myArray.length) {
+//     console.log(` the output is ${myArray[index]}`);
+// index = index + 1
+// }
+
+let balance = 11
+do {
+    console.log(`the balance is ${balance}`);
+    balance = balance+1
+} while (balance<=10);
