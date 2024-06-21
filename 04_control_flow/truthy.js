7c25db7c3c2eed85afdc8206bfcafb32a2c1c091
MD GULAM RASUL KHAN <130956007+mdgulamrasulkhan@users.noreply.github.com>
2024-06-21 05:47:39 +0000

    truthy,nullish & terniary operator

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,47 @@
+// const userEmail = "khan@gmail.com"
+// if(userEmail){
+//     console.log("got an email");
+// }
+// else
+// console.log("don't ge an eamil");
+
+const userEmail = {}
+// if(userEmail){
+    // console.log("got an email");
+// }
+// else
+// console.log("don't ge an eamil");
+
+
+// falsy values
+//  0, -0, "", false, null, undefined, NaN
+// truthy values
+// "0", 'false' , " ", [], {}, function(){}
+
+// if(Object.keys(userEmail) == 0){
+//     console.log("object is empty");
+// }
+
+// nullish coalecsing operator
+// const num1 = 2??5
+// const num2 = null??10
+// num3 = undefined??5??8
+// const num4 = null??9??20
+
+
+
+// console.log(num1)
+// console.log(num2)
+// console.log(typeof num3 ," : " ,  num3)
+// console.log(num4);
+
+// terniary operator
+
+// condition ? true : false // syntax
+
+const price = 100
+  price ==80 ? console.log("less than 80") : console.log("more than 80");
+
+
+
+  
