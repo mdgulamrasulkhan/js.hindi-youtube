0241ede99e7c6793016099a8ab83dc84c7b45589
MD GULAM RASUL KHAN <130956007+mdgulamrasulkhan@users.noreply.github.com>
2024-06-23 04:10:50 +0000

    filter, map,

---
+++ 05_iteration/07_seven.js
@@ -0,0 +1,24 @@
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+// const mynewnum =myNumbers.map((num) => num+10)
+// console.log(mynewnum);
+// const myNewnum = myNumbers.forEach((num)=> num + 10)
+// console.log(myNewnum);
+
+// const MyArr = []
+// const myNewnum = myNumbers.forEach((num) => { 
+//    num = num + 10
+//      return MyArr.push(num)})
+   
+
+//  console.log(MyArr);
+               
+     /* chaining 
+
+const newNums = myNumbers
+                         .map((num => num*10))
+                         .map((num) => num +1)
+                         .filter((num => num>= 40))
+                         console.log(newNums);  */
+   
+                         
+
